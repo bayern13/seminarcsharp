7df34f6c3694e31a380ee070a3bdd53fcae04540
Oleg <bayernkawasaki@gmail.com>
2023-09-19 20:04:52 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.WriteLine("Введите первое число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число : ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2 && number1 > number3)
+    Console.Write($"max = {number1}");
+else if (number2 > number1 && number2 > number3)
+Console.Write($"max = {number2}");
+else 
+Console.Write($"max = {number3}");
