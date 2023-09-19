5868b0b57109daf7318925ee21cc269806500522
Oleg <bayernkawasaki@gmail.com>
2023-09-19 17:54:38 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write($"max = {number1}");
+
+}
+else Console.Write($"max = {number2}");
