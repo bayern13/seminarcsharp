4882f3a39acd4d808438d37c8e23060922933e12
Oleg <bayernkawasaki@gmail.com>
2023-09-25 17:06:12 +0300

    Task15

---
+++ seminar2csharp/Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("выходной");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("это не день недели");
+    }
+    else Console.WriteLine("выходной");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
