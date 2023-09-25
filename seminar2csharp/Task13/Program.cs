d1ccfee9976b936be435abeb43aa43e9df52c8d1
Oleg <bayernkawasaki@gmail.com>
2023-09-25 16:53:14 +0300

    Task13

---
+++ seminar2csharp/Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿int ThirdDigit(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result;
+}
+Console.Write("Введите число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (ThirdDigit(number1) == -1)
+    Console.WriteLine("третьей цифры нет");
+else
+    Console.WriteLine($"Третья цифра {ThirdDigit(number1)}");
