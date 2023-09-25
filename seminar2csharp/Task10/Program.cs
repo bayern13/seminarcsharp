04265b97eb5d0bb4c62cb03ce5583dbb88cbed29
Oleg <bayernkawasaki@gmail.com>
2023-09-25 16:19:28 +0300

    Task010

---
+++ seminar2csharp/Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, 
+// которая принимает на вход 
+// трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число : ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int deleteDigits = DeleteDigits(secondNumber);
+Console.WriteLine($"{secondNumber} -> {deleteDigits}");
+
+int DeleteDigits(int num)
+{
+    int findSecondDigit = num / 10;
+    return findSecondDigit % 10;
+
+}
