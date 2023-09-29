76b2fef0df63d7320646eff2cc8e8794ae4a56de
Oleg <bayernkawasaki@gmail.com>
2023-09-29 15:12:50 +0300

    Task25

---
+++ seminar2csharp/Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+string GetCoordinatesRange(int num)
+{
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x < 0, y > 0";
+    if (num == 3) return "x < 0 && y < 0";
+    if (num == 4) return "x > 0 && y < 0";
+    return null;
+}
+Console.WriteLine("Введите номер четверти :");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+
+string getCoordinatesRange = GetCoordinatesRange(quarter);
+
+string result = getCoordinatesRange != null ? $"Заданному номеру четверти соответствует  диапазон {getCoordinatesRange}"
+                                         : "Введены неккоректные результаты";
+ Console.WriteLine(result);
---
+++ seminar2csharp/Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿
+double NaturalDegree(double num1,double num2)
+{
+
+    double res = Math.Pow(num1, num2);
+    return res;
+}
+
+
+
+Console.WriteLine("Введите первое число : ");
+double number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+double number2 = Convert.ToInt32(Console.ReadLine());
+
+double naturalDegree = NaturalDegree(number1, number2);
+Console.WriteLine($"{number1},{number2} -> {naturalDegree}");
