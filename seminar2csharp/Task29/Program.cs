7b71297d79468c47595366387381621c88935584
Oleg <bayernkawasaki@gmail.com>
2023-10-02 01:37:37 +0300

    Task29

---
+++ seminar2csharp/Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array =  new int[8];
+    Random rnd = new Random();
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = rnd.Next(0,7); //(0, 2)
+        
+    }
+return array;
+
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", "); //(0, 2)
+        
+    }
+}
+
+int[] arr = CreateArray(1);
+PrintArray(arr);
+
