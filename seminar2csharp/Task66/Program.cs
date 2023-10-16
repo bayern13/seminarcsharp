3574c757c4aa060b1b57077c9ba990a0e7e23dd7
Oleg <bayernkawasaki@gmail.com>
2023-10-16 21:52:58 +0300

    Task66

---
+++ seminar2csharp/Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+
+int SumOfNaturalNumbers(int m, int n)
+{
+    if (m == n) return m;
+    return m + SumOfNaturalNumbers(m + 1, n);
+
+}
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(SumOfNaturalNumbers(m, n));
